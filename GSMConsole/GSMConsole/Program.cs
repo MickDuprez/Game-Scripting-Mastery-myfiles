f84b3c337f3cf6fd47a54b7922f0c7d6a75eecbd
Mick Duprez <mickduprez@gmail.com>
2017-06-24 10:59:29 +1000

    build hello world

--- GSMConsole/GSMConsole/Program.cs
+++ GSMConsole/GSMConsole/Program.cs
@@ -7,6 +7,8 @@ namespace GSMConsole {
     class Program {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
         }
     }
 }
